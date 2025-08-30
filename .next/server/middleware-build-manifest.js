098d9c11b37e5c97cd6685e92a87e95c061684dc
Ibe Arua <stanarua@aol.com>
2025-08-30 17:27:51 +0100

    Adding delivery

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -23,16 +23,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/prescription-upload": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/prescription-upload.js"
-    ],
-    "/store": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/store.js"
     ]
   },
   "ampFirstPages": []
